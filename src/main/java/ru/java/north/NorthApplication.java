6022d5d727d77a7cb57d18c98451284846ebb429
Mamedov <vagif_m@bk.ru>
2023-10-05 21:39:09 +0300

    git init

---
+++ src/main/java/ru/java/north/NorthApplication.java
@@ -0,0 +1,11 @@
+package ru.java.north;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NorthApplication.class, args);
+    }
+}
